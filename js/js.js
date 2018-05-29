8605eec0900b1afd7218024adab5e7edc3676470
Stanislav <39649166+stasmihalkv@users.noreply.github.com>
2018-05-29 23:44:49 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,12 @@
+
+  $('.sl').slick({
+        infinite: true,
+        centerPadding:'400px',
+        centerMode: true,
+        dots: true,
+        arrows:true,
+        });
+
+
+//    prevArrow: '<img src="/css/arrow-left.png">',
+//    prevArrow: '<img src="/css/arrow-right.png">',  
