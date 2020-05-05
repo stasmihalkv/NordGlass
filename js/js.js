c4ba104f2fd20e60a389860e1ef462e22c0e6d70
Stanislav <39649166+stasmihalkv@users.noreply.github.com>
2020-05-06 00:33:15 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,12 +1,11 @@
-
-  $('.sl').slick({
+window.onload = function(){
+      $('.sl').slick({
         infinite: true,
         centerPadding:'400px',
         centerMode: true,
         dots: true,
         arrows:true,
         });
+    
+}
 
-
-//    prevArrow: '<img src="/css/arrow-left.png">',
-//    prevArrow: '<img src="/css/arrow-right.png">',  
